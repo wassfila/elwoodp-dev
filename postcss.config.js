9e040817e4166e6f811f203a90791b2e7b59403b
Elwood-P <info@pjlittlewood.co.uk>
2022-09-13 13:18:19 +0100

    Install Postcss functions and add toRem function

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,24 @@
+// module.exports = {
+//   plugins: [
+//     require('postcss-import'),
+//     require('postcss-mixins'),
+//     require('postcss-nested'),
+//     require('postcss-simple-vars'),
+//     require('postcss-functions'),
+//   ],
+// };
+
+const toRem = (value) => {
+  // return `${(value/16)}rem`;
+  return value / 16;
+};
+
 module.exports = {
-  plugins: [
-    require('postcss-import'),
-    require('postcss-mixins'),
-    require('postcss-nested'),
-    require('postcss-simple-vars'),
-  ],
+  plugins: {
+    'postcss-import': {},
+    'postcss-mixins': {},
+    'postcss-nested': {},
+    'postcss-simple-vars': {},
+    'postcss-functions': { functions: { toRem: toRem } },
+  },
 };
